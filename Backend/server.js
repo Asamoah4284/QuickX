3196974d18e37ad4a347403c836a7a030ab280f4
Richard Asamoah <richardasamoah284@gmail.com>
2025-05-05 13:24:28 +0000

    s

--- Backend/server.js
+++ Backend/server.js
@@ -9,18 +9,10 @@ require('dotenv').config();
 const app = express();
 
 
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://www.quickxlearn.com'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  }));
 
 // Middleware
 app.use(cors());
-
 app.use(express.json());
-
-
 
 // Database connection
 console.log('Attempting to connect to MongoDB...');
